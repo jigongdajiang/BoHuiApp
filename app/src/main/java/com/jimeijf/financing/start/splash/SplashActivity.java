d73907afeb6b3c3a7b32e21ac5b752fc6d4e23a7
gaojigong <gaoji_gong@126.com>
2017-11-29 16:18:57 +0800

    first

--- app/src/main/java/com/jimeijf/financing/start/splash/SplashActivity.java
+++ app/src/main/java/com/jimeijf/financing/start/splash/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.jimeijf.financing.start.splash;
 
+import com.jimeijf.core.util.StatusBarCompatUtil;
 import com.jimeijf.financing.R;
 import com.jimeijf.financing.common.activity.AbsBaseActivity;
 
@@ -8,10 +9,20 @@ import com.jimeijf.financing.common.activity.AbsBaseActivity;
  * @date : 2017/11/17
  * FileName:
  * @description:
+ *
+ * 防止从后台进入每次都进入启屏页
+ * 全屏模式
+ *
  */
 
 
 public class SplashActivity extends AbsBaseActivity<SplashPresenter,SplashModel> implements SplashContact.View {
+    @Override
+    protected void doBeforeSetContentView() {
+        super.doBeforeSetContentView();
+        new StatusBarCompatUtil.Builder(this).setSupportType(2).builder().apply();
+    }
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_splash;
